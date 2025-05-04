3f2cd7133647c72f29124e9cda20a51b405bfb0f
Skagit <skhemasinghe666@gmail.com>
2025-05-04 06:23:50 +0530

    Added delete class feature

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# In-memory list to hold schedule entries temporarily
+schedule = []
+
 @app.route('/')
 def home():
-    return "Hello, this is your Document Management System!"
+    return render_template('index.html', schedule=schedule)
+
+@app.route('/add', methods=['POST'])
+def add_class():
+    subject = request.form['subject']
+    teacher = request.form['teacher']
+    date = request.form['date']
+    time = request.form['time']
+
+    schedule.append({
+        'subject': subject,
+        'teacher': teacher,
+        'date': date,
+        'time': time
+    })
+
+    return redirect('/')
+
+@app.route('/delete/<int:index>', methods=['POST'])
+def delete_class(index):
+    if 0 <= index < len(schedule):
+        del schedule[index]
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
