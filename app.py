b0586abf95373ada88870c49ff72566dbb61debb
Skagit <skhemasinghe666@gmail.com>
2025-05-04 08:05:59 +0530

    Added validations

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from datetime import date
 
 app = Flask(__name__)
 
@@ -7,14 +8,24 @@ schedule = []
 
 @app.route('/')
 def home():
-    return render_template('index.html', schedule=schedule)
+    min_date = date.today().isoformat()
+    return render_template('index.html', schedule=schedule,min_date=min_date)
+    
+
 
 @app.route('/add', methods=['POST'])
 def add_class():
-    subject = request.form['subject']
-    teacher = request.form['teacher']
-    date = request.form['date']
-    time = request.form['time']
+    subject = request.form.get('subject','').strip()
+    teacher = request.form.get('teacher','').strip()
+    date = request.form.get('date','').strip()
+    time = request.form.get('time','').strip()
+
+    if not subject or not teacher or not date or not time:
+     return "All fields are required",400 #bad request
+
+    for cls in schedule:
+     if cls['subject'] == subject and cls['date'] == date and cls['time'] == time:
+      return "This class is already scheduled at that time!", 400
 
     schedule.append({
         'subject': subject,
